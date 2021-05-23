b6d1172a528c221db55026f6db22033c6e079524
EQalityn <egor120202@gmail.com>
2021-05-23 12:51:26 +0300

    feat: add servlets

---
+++ src/main/java/bsu/rfe/Kalitin/lab8/servlets/MessageListServlet.java
@@ -0,0 +1,34 @@
+package bsu.rfe.Kalitin.lab8.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import bsu.rfe.Kalitin.lab8.entity.ChatMessage;
+
+public class MessageListServlet extends ChatServlet {
+    private static final long serialVersionUID = 1L;
+    final int value = 1000;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Установить кодировку HTTP-ответа UTF-8
+        response.setCharacterEncoding("utf8");
+        // Получить доступ к потоку вывода HTTP-ответа
+        PrintWriter pw = response.getWriter();
+        // Записть в поток HTML-разметку страницы
+        pw.println("<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><meta http-equiv='refresh' content='10'></head>");
+        pw.println("<body>");
+        // В обратном порядке записать в поток HTML-разметку для каждого сообщения
+
+        for (int i = 0; i< messages.size(); i++) {
+            ChatMessage aMessage = messages.get(i);
+            pw.println("<div><b> -> (" + aMessage.getCertainTime() + " ) " + "</b> ");
+            pw.println(aMessage.getAuthor().getName() + "</strong>: " + aMessage.getMessage() + "</div>");
+        }
+        pw.println("</body></html>");
+    }
+}
--- src/main/java/com/example/Lab_8/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.Lab_8;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
